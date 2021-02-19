435f46c0b74d1c59f6c56f19ab60f9930e8eeee9
user1 <user1@DESKTOP-2EG5R88>
2021-02-19 14:45:46 +0900

    eclipse올립니다!

---
+++ springtest/src/main/java/com/github/push/test/Test01.java
@@ -0,0 +1,19 @@
+package com.github.push.test;
+
+public class Test01 {
+
+	String userId;
+	String userName;
+	String userPw;
+	String userGender;
+	public String getUserId() {		return userId;	}
+	public void setUserId(String userId) {		this.userId = userId;	}
+	public String getUserName() {		return userName;	}
+	@Override
+	public String toString() {		return super.toString();	}
+	public void setUserName(String userName) {		this.userName = userName;	}
+	public String getUserPw() {		return userPw;	}
+	public void setUserPw(String userPw) {		this.userPw = userPw;	}
+	public String getUserGender() {		return userGender;	}
+	public void setUserGender(String userGender) {		this.userGender = userGender;	}
+}
